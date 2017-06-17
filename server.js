e528372f6c20e48d71f5176888a6a03dbb30b338
user <user@users-Mac.local>
2017-06-17 20:48:53 +0200

    Added /dump

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => res.send({
     message: "Please check the API docs at https://github.com/cocktinder/api"
 }));
 
+app.get("/dump", (req, res) => mongo(process.env.MONGO_URL).cocktails.find((err,data) => {
+    res.send(data);
+}));
+
 
 
 app.listen(process.env.PORT || 5000, () => console.log("Online at :" + process.env.PORT || 5000));
